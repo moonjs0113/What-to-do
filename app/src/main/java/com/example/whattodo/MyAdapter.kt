82fa86309d1ac66c3a8f4a6bf42ed6408faeaa8b
Kwon Mingyu <kmgmg230601@gmail.com>
2023-05-15 13:34:30 +0900

    기본적인 리사이클려 뷰를 만들었습니다. 리사이클려 뷰에 들어가는 viewHolder의 디자인은 제가 임시로 만들었고, 우선도를 구하는 공식도 제가 임의로 만들었습니다. 그리고 recylerview 가 어떻게 나오는지 보기 위해 priority fragment 에 한번 넣어 보았습니다. (단지 시연용이기에 추후에 prioirty fragment를 작업하 실 때는 삭제해 주시길 부탁드립니다)

---
+++ app/src/main/java/com/example/whattodo/MyAdapter.kt
@@ -0,0 +1,94 @@
+package com.example.whattodo
+
+import android.graphics.Color
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.whattodo.databinding.SimpleViewHolderBinding
+import java.util.*
+import android.graphics.drawable.GradientDrawable as GradientDrawable1
+
+
+// 우선 제가 간단히 만들어 놓은 예시용 디자인을 binding 했습니다.
+// binding을 바꾸고 싶으시다면 1,2,3,4,5 binding 부분을 바꿔주세요
+
+class MyAdapter(val items:ArrayList<ToDo>)
+    : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    interface OnItemClickListener{
+        fun OnItemClick(position : Int)
+        fun OnItemLongClick(position : Int) : Boolean // 꾹 눌렀을 때 반응할 콜백 함수는 Boolean 값을 반환해 줘야 합니다
+    }
+
+    interface OnCalculatePriorityListener{ // 우선도 정해주는 함수를 외부로 부터 받습니다
+        fun calculatePriority( _importance : Int, _timeLeft : Long, _time_taken : Float) : Float
+        // importance : 중요도
+        // timeLeft : 현재 시간에서 마감일을 뺀 기간. 초 단위로 인수가 넘어갑니다
+        // time_taken : 일을 하는 데 걸리는 시간
+        
+        // 최종 우선도를 float으로 반환합니다
+    }
+
+    var itemClickListener: OnItemClickListener? = null // 그냥 눌렀을 때 반응할 Listener
+    var itemLongClickListener : OnItemClickListener? = null // 꾹 눌렀을 때 반응할 Listener
+    var calculatePriorityListener : OnCalculatePriorityListener? = null // 우선도 정해주는 함수를 외부로 부터 받습니다
+
+    inner class MyViewHolder(val binding: SimpleViewHolderBinding) : RecyclerView.ViewHolder(binding.root) //////////////////// 1 /////////////////////////
+    {  
+        init{
+            binding.touchableLayout.setOnClickListener{//////////////////// 2 /////////////////////////
+                itemClickListener?.OnItemClick(adapterPosition)
+            }
+
+            binding.touchableLayout.setOnLongClickListener{ //////////////////// 3 /////////////////////////
+                itemLongClickListener?.OnItemLongClick(adapterPosition)!!
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view : SimpleViewHolderBinding = SimpleViewHolderBinding.inflate(LayoutInflater.from(parent.context)) //////////////////// 4 /////////////////////////
+        return MyViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) { //////////////////// 5 /////////////////////////
+        holder.binding.explanation.text = items[position].explanation
+        holder.binding.importance.text = "중요도: " + items[position].importance.toString()
+
+        // Date.getTime() 은 해당날짜를 기준으로1970년 00:00:00 부터 몇 초가 흘렀는지를 반환해준다.
+        val calDate: Long = items[position].deadLine.getTime() - Date(System.currentTimeMillis()).getTime()
+
+        // 이제 24*60*60*1000(각 시간값에 따른 차이점) 을 나눠주면 일수가 나온다.
+        var calDateDays = calDate / (24 * 60 * 60 * 1000)
+        calDateDays = Math.abs(calDateDays)
+        holder.binding.deadLine.text = "마감 " + calDateDays.toString() + "일 전"
+
+        val priority : Float =  calculatePriorityListener?.calculatePriority(items[position].importance , calDate, items[position].time_taken)!!
+
+
+        if(priority > 50)
+        {
+            holder.binding.priorityImageView.setColorFilter(Color.RED)
+        }else if(priority > 30)
+        {
+            holder.binding.priorityImageView.setColorFilter(Color.YELLOW)
+        }else if(priority > 0)
+        {
+            holder.binding.priorityImageView.setColorFilter(Color.GREEN)
+        }else // 우선도가 음수 -> 마감기한 놓침
+        {
+            holder.binding.priorityImageView.setColorFilter(Color.TRANSPARENT)
+        }
+
+
+        //////////////////// 5 /////////////////////////
+
+    }
+
+}
