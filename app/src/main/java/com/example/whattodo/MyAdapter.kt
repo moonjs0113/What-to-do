8bd066f383f9b222155773cf9e23d69c3cc45a2c
Kwon Mingyu <kmgmg230601@gmail.com>
2023-05-28 22:05:11 +0900

    ToDo Data 클래스에 priority를 추가하였습니다. 이건 입력 할 때는 그냥 0으로 주면 recyclerview안에서 자체적으로 계산 후에 알아서 채워줍니다. 확장 기능으로는, 중요도 색깔 바꾸기, 중요도 공식 바꾸기, 중요도에 따라 정렬 하기등을 추가 하였습니다

--- app/src/main/java/com/example/whattodo/MyAdapter.kt
+++ app/src/main/java/com/example/whattodo/MyAdapter.kt
@@ -7,19 +7,85 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.whattodo.databinding.SimpleViewHolderBinding
 import java.util.*
+import kotlin.Comparator
 import android.graphics.drawable.GradientDrawable as GradientDrawable1
 
+// 확장성 기능 1 : recyclerView의 우선도 색깔을 바꾸고 싶은 경우에는 adapter.setPriorityColor()로 설정 해 주세요
 
-// 우선 제가 간단히 만들어 놓은 예시용 디자인을 binding 했습니다.
-// binding을 바꾸고 싶으시다면 1,2,3,4,5 binding 부분을 바꿔주세요
+// 확장성 기능 2 : recyclerView의 우선도 공식을 바꾸고 싶은 경우에는 adapter.calculatePriorityListener = object : 형식으로 OnCalculatePriorityListener 익명 객체를 넣어주세요
+/////////////// 우선도를 바꾼 뒤에는 notifyDataSetChanged을 불러주세요!! /////////////////
+
+
+// 확장성 기능 3 : recyclerView의 클락 시의 행동을 바꾸고 싶은 경우에는 adapter.itemClickListener = object : 형식으로 OnItemClickListener 익명 객체를 넣어주세요
+// 확장성 기능 4 : recyclerView의 긴 클릭 시의 행동을 바꾸고 싶은 경우에는 adapter.itemLongClickListener = object : 형식으로 OnLongItemClickListener 익명 객체를 넣어주세요
 
 class MyAdapter(val items:ArrayList<ToDo>)
     : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
+    //각각 빨간색, 노란색, 초록색을 담고 있습니다
+    // 만약 색깔을 바꾸고 싶다면 이 arrayList 의 값을 바꿔주면 그 색깔이 반영이 됩니다
+    var hexColors = arrayListOf<String>("e03b22", "eff238", "24f064")
 
+    //////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////adapter.메소드이름() 로 접근 해 주세요///////////////////////////
+
+    // 6자리 hex 색깔을 문자열로 보내주면 바뀝니다. 기본값은 각 각 빨강, 노랑, 초록이지만, 이 함수를 call하면 넘겨진 색깔에 맞게 다 변하게 됩니다
+    fun setPriorityColor(veryImportantColor : String, ImportantColor : String, notImportantColor : String)
+    {
+        hexColors[0] = veryImportantColor
+        hexColors[1] = ImportantColor
+        hexColors[2] = notImportantColor
+        notifyDataSetChanged()
+    }
+
+    // 이걸 호출하면 리사이클러 뷰 안의 아이템들이 우선도 오름차순으로 정렬이 됩니다. -> 초록색일 수록 위에, 빨간색 일 수록 아래에 깔리게 됩니다
+    fun sortItemwithAscendingPriority()
+    {
+        val comparator: Comparator<ToDo> = object : Comparator<ToDo> {
+            override fun compare(o1: ToDo?, o2: ToDo?): Int {
+
+                return if(o1!!.priority - o2!!.priority < 0) {
+                    -1
+                }else if(o1!!.priority - o2!!.priority > 0) {
+                    1
+                }else {
+                    0
+                }
+            }
+
+        }
+        items.sortWith(comparator)
+        notifyDataSetChanged()
+    }
+
+    // 이걸 호출하면 리사이클러 뷰 안의 아이템들이 우선도 내림차순으로 정렬이 됩니다. -> 빨간색일 수록 위에, 초록색 일 수록 아래에 깔리게 됩니다
+    fun sortItemwithDescendingPriority()
+    {
+        val comparator: Comparator<ToDo> = object : Comparator<ToDo> {
+            override fun compare(o1: ToDo?, o2: ToDo?): Int {
+                return if(o1!!.priority - o2!!.priority > 0) {
+                    -1
+                }else if(o1!!.priority - o2!!.priority < 0) {
+                    1
+                }else {
+                    0
+                }
+            }
+
+        }
+        items.sortWith(comparator)
+        notifyDataSetChanged()
+    }
+
+
+    /////////////////////adapter.메소드이름() 로 접근 해 주세요///////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////
 
     interface OnItemClickListener{
         fun OnItemClick(position : Int)
+    }
+
+    interface OnLongItemClickListener{
         fun OnItemLongClick(position : Int) : Boolean // 꾹 눌렀을 때 반응할 콜백 함수는 Boolean 값을 반환해 줘야 합니다
     }
 
@@ -33,25 +99,25 @@ class MyAdapter(val items:ArrayList<ToDo>)
     }
 
     var itemClickListener: OnItemClickListener? = null // 그냥 눌렀을 때 반응할 Listener
-    var itemLongClickListener : OnItemClickListener? = null // 꾹 눌렀을 때 반응할 Listener
+    var itemLongClickListener : OnLongItemClickListener? = null // 꾹 눌렀을 때 반응할 Listener
 
     var calculatePriorityListener : OnCalculatePriorityListener? = null // 우선도 정해주는 함수를 외부로 부터 받습니다
 
-    inner class MyViewHolder(val binding: SimpleViewHolderBinding) : RecyclerView.ViewHolder(binding.root) //////////////////// 1 /////////////////////////
+    inner class MyViewHolder(val binding: SimpleViewHolderBinding) : RecyclerView.ViewHolder(binding.root)
     {  
         init{
-            binding.touchableLayout.setOnClickListener{//////////////////// 2 /////////////////////////
+            binding.touchableLayout.setOnClickListener{
                 itemClickListener?.OnItemClick(adapterPosition)
             }
 
-            binding.touchableLayout.setOnLongClickListener{ //////////////////// 3 /////////////////////////
+            binding.touchableLayout.setOnLongClickListener{
                 itemLongClickListener?.OnItemLongClick(adapterPosition)!!
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view : SimpleViewHolderBinding = SimpleViewHolderBinding.inflate(LayoutInflater.from(parent.context), parent, false) //////////////////// 4 /////////////////////////
+        val view : SimpleViewHolderBinding = SimpleViewHolderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MyViewHolder(view)
     }
 
@@ -60,7 +126,7 @@ class MyAdapter(val items:ArrayList<ToDo>)
 
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) { //////////////////// 5 /////////////////////////
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.binding.explanation.text = items[position].explanation
         holder.binding.importance.text = "중요도: " + items[position].importance.toString()
 
@@ -78,6 +144,8 @@ class MyAdapter(val items:ArrayList<ToDo>)
 
 
         val priority : Float =  calculatePriorityListener?.calculatePriority(items[position].importance , calDate, items[position].time_taken)!!
+        items[position].priority = priority
+
         if(priority > 50)
         {
             var priorityGap = 100 - priority
@@ -97,7 +165,7 @@ class MyAdapter(val items:ArrayList<ToDo>)
                 hex = "0$hex"
             }
 
-            holder.binding.priorityImageView.setColorFilter(Color.parseColor("#${hex}e03b22"))
+            holder.binding.priorityImageView.setColorFilter(Color.parseColor("#${hex}${hexColors[0]}"))
         }else if(priority > 20)
         {
             val priorityGap = priority - 20
@@ -108,7 +176,7 @@ class MyAdapter(val items:ArrayList<ToDo>)
                 hex = "0$hex"
             }
 
-            holder.binding.priorityImageView.setColorFilter(Color.parseColor("#${hex}eff238"))
+            holder.binding.priorityImageView.setColorFilter(Color.parseColor("#${hex}${hexColors[1]}"))
         }else if(priority > 0)
         {
             val priorityGap = priority
@@ -119,7 +187,7 @@ class MyAdapter(val items:ArrayList<ToDo>)
                 hex = "0$hex"
             }
 
-            holder.binding.priorityImageView.setColorFilter(Color.parseColor("#${hex}24f064"))
+            holder.binding.priorityImageView.setColorFilter(Color.parseColor("#${hex}${hexColors[2]}"))
 
         }else // 우선도가 음수 -> 마감기한 놓침
         {
@@ -127,7 +195,6 @@ class MyAdapter(val items:ArrayList<ToDo>)
         }
 
 
-        //////////////////// 5 /////////////////////////
 
     }
 
--- app/src/main/java/com/example/whattodo/ToDo.kt
+++ app/src/main/java/com/example/whattodo/ToDo.kt
@@ -2,7 +2,7 @@ package com.example.whattodo
 
 import java.util.*
 
-data class ToDo (var explanation : String, var deadLine : Date, var time_taken : Float , var importance : Int )
+data class ToDo (var explanation : String, var deadLine : Date, var time_taken : Float , var importance : Int , var priority : Float)
 // explanation : 할 일에 대한 설명
 // deadLine : 마감일
 // time_taken : 소요 시간
