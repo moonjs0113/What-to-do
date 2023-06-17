97d267d301c3ba597793843f7d065d187c9bcdf7
moonjs0113 <littlemoom@naver.com>
2023-06-18 01:53:17 +0900

    Setting

--- app/src/main/java/com/example/whattodo/manager/room/ToDoDAO.kt
+++ app/src/main/java/com/example/whattodo/manager/room/ToDoDAO.kt
@@ -17,7 +17,12 @@ interface ToDoDAO {
     @Query("SELECT * FROM todos")
     fun getAllTodo(): List<ToDo>
 
-
     @Query("SELECT * FROM todos WHERE explanation LIKE '%' || :keyword || '%'")
     fun findToDo(keyword: String): List<ToDo>
+
+    @Query("DELETE FROM todos")
+    fun deleteAllTodo()
+
+    @Query("DELETE FROM todos WHERE isComplete = 1")
+    fun deleteCompletedTodo()
 }
